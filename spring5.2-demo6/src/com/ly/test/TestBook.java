a839aac37e78f646751ae79654a3a535e26b103b
lyy320617627 <ly320617627@163.com>
2021-10-19 13:06:21 +0800

    GenericApplicationContext整合日志

--- spring5.2-demo6/src/com/ly/test/TestBook.java
+++ spring5.2-demo6/src/com/ly/test/TestBook.java
@@ -6,8 +6,12 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.lang.Nullable;
 
 public class TestBook {
+    @Nullable
+    private String bookName;
     @Test
     public void testAccount(){
         ApplicationContext context=new ClassPathXmlApplicationContext("bean1.xml");
@@ -26,4 +30,17 @@ public class TestBook {
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
     }
+      //函数式风格创建对象，交给spring进行管理
+    @Test
+    public void testGenericApplicationContext(){
+        //创建GenericApplicationContext对象
+        GenericApplicationContext genericApplicationContext=new AnnotationConfigApplicationContext();
+        //2.调用context的方法对象注册
+        genericApplicationContext.refresh();
+        genericApplicationContext.registerBean("user1",User.class,()->new User());
+        //3.获取在spring注册的对象
+        Object bean = genericApplicationContext.getBean("user1");
+        System.out.println(bean);
+    }
+
 }
---
+++ spring5.2-demo6/src/com/ly/test/User.java
@@ -0,0 +1,5 @@
+package com.ly.test;
+
+public class User {
+
+}
